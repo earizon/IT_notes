ba374d0375fe254cf23ef28cc0d226835b1fd29f
Enrique Arizón Benito <enrique.arizon.benito@everis.com>
2018-04-07 14:27:02 +0000

    Fix maps for maven, vertx. Update create_index.html

--- create_index.sh
+++ create_index.sh
@@ -10,14 +10,15 @@ Pseudo-ordered notes and quick-sheets on software development and system archite
 If this project was useful to you, consider making a donation to my Bitcoin address (1JXPKbYJwfbMfFCWbRdiKRgw8H2JaPboUK):<br/>
 <image style='width:100px' src='./btc_address_1JXPKbYJwfbMfFCWbRdiKRgw8H2JaPboUK.png' />
 </p>
-<ul>
 EOF
 REGEX_IGNORE=`cat ignore_list`
-find -name "*html"  -type f | egrep -v "${REGEX_IGNORE}" | while read f ; do
-echo "<li><a href='${f}'>${f}</a></li>" >>index.html
+find -name "*html"  -type f | egrep -v "${REGEX_IGNORE}" | sort | while read f ; do
+title=`head $f | grep "<title>" | sed "s/<title>//g" | sed "s/<.title>//g" `
+echo $title
+location=`echo $f | cut -f 2 -d "/" `
+echo "<code>${location}/</code> <a href='${f}'>${title}</a><br/>" >>index.html
 done
 cat << EOF >>index.html
-</ul>
 </body>
 </html>
 EOF

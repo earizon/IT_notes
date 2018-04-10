8438c26dfc18d0981c40ca10e3dfecfd5e8de93a
Enrique Arizón Benito <enrique.arizon.benito@everis.com>
2018-04-10 14:09:09 +0000

    Big big refactor, finally reuse js/css

--- create_index.sh
+++ create_index.sh
@@ -6,17 +6,17 @@ cat << EOF >index.html
 <h1>IT_notes</h1>
 
 <p>
-Pseudo-ordered notes and quick-sheets on software development and system architecture and administration I write down while working on different projects.<br/>
+detailed mental maps on software development and system architecture and administration I write down while working on different projects.<br/>
 If this project was useful to you, consider making a donation to my Bitcoin address (1JXPKbYJwfbMfFCWbRdiKRgw8H2JaPboUK):<br/>
 <image style='width:100px' src='./btc_address_1JXPKbYJwfbMfFCWbRdiKRgw8H2JaPboUK.png' />
 </p>
 EOF
-REGEX_IGNORE=`cat ignore_list`
+# REGEX_IGNORE=`cat ignore_list`
+REGEX_IGNORE="index"
 find -name "*html"  -type f | egrep -v "${REGEX_IGNORE}" | sort | while read f ; do
 title=`head $f | grep "<title>" | sed "s/<title>//g" | sed "s/<.title>//g" `
-echo $title
 location=`echo $f | cut -f 2 -d "/" `
-echo "<code>${location}/</code> <a href='${f}'>${title}</a><br/>" >>index.html
+echo $title  | grep "ignore" || echo "<code>${location}/</code> <a href='${f}'>${title}</a><br/>" >>index.html
 done
 cat << EOF >>index.html
 </body>

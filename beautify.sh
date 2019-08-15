22cfecdc7d29883c649afffa4bfd7321a4fd533d
Enrique Arizon Benito <enrique.arizonbenito@gmail.com>
2019-08-15 17:33:02 +0000

    add script doc

--- beautify.sh
+++ beautify.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+
+# Convert this text:
+#
+# /----v----\
+# |    |    |
+# }----+----{
+# |    |    |
+# \_---^---_/
+#
+# Into this one 
+# ┌────┬────┐
+# │    │    │
+# ├────┼────┤
+# │    │    │
+# └────┴────┘
+#
+# Just pipe it through beautify!
+#
+# It will also convert conflictive html characters
+# (< > and &) into  similar UTF-8 non-conflictive
+# ones. (Don't worry, the SPB javascript file will
+# convert them back before rendering in the browser)
+# 
 sed "s/[/]-/┌─/g" | \
 \
 sed "s/-[\]/─┐/g" | \

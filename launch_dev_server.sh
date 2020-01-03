d403d0fb704ac430926e4adc0043061ffd144b9c
Enrique Arizón Benito <enrique.arizonbenito@gmail.com>
2020-01-03 02:04:24 +0100

    Listen on all interfaces

--- launch_dev_server.sh
+++ launch_dev_server.sh
@@ -1,5 +1,5 @@
 if [ -x /usr/bin/python3 ]; then
-   python3 -m http.server  --bind 127.0.0.1 8000 1>/tmp/port8000.log 2>&1 &
+   python3 -m http.server  --bind 0.0.0.0 8000 1>/tmp/port8000.log 2>&1 &
 else 
    python2 launch_dev_server.py 1>/tmp/port8000.log 2>&1 &
 

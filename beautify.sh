06a5961d0f3bdb2dc40e23cfa89695455f9cdccc
Enrique Arizón Benito <enrique.arizonbenito@gmail.com>
2020-06-07 13:21:37 +0200

    Update beautify.sh script

--- beautify.sh
+++ beautify.sh
@@ -1,68 +1,20 @@
 #!/bin/bash
-
-# Convert this text:
-#
-# /----v----\
-# |    |    |
-# }----+----{
-# |    |    |
-# \_---^---_/
-#
-# Into this one 
-# ┌────┬────┐
-# │    │    │
-# ├────┼────┤
-# │    │    │
-# └────┴────┘
-#
-# Just pipe it through beautify!
-#
-# It will also convert conflictive html characters
-# (< > and &) into  similar UTF-8 non-conflictive
-# ones. (Don't worry, the SPB javascript file will
-# convert them back before rendering in the browser)
-# 
 sed "s/[/]-/┌─/g" | \
-\
-sed "s/[+]-/┼─/g" | \
-\
 sed "s/-[\]/─┐/g" | \
-\
-sed "s/-[+]/─┼/g" | \
-\
 sed "s/_[/]/─┘/g" | \
-\
-sed "s/_[+]/─┼/g" | \
-\
 sed "s/[\]_/└─/g" | \
-\
-sed "s/[+]_/┼─/g" | \
-\
+sed "s/-[/]/─┘/g" | \
+sed "s/[\]-/└─/g" | \
 sed "s/}-/├─/g" | \
-\
 sed "s/-{/─┤/g" | \
-\
 sed "s/-v-/─┬─/g" | \
-\
 sed "s/-^-/─┴─/g" | \
-\
 sed "s/-x-/─┼─/g" | \
-\
-sed "s/-+-/─┼─/g" | \
-\
+sed "s/-|-/─┼─/g" | \
 sed "s/->/─→/g" | \
-\
 sed "s/<-/←─/g" | \
-\
 sed "s/-/─/g" | \
-\
 sed "s/|/│/g" | \
-\
-sed "s/-+-/─┼─/g" | \
-\
-sed "s/</˂/g" | \
-\
-sed "s/>/˃/g" | \
-\
-sed "s/&/⅋/g"
-
+sed "s/-+-/-┼-/g" |\
+sed "s/^>/├/g" | \
+sed "s/<$/┤/g"

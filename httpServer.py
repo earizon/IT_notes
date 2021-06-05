654f807894050b3b8f60738a671258349255f47d
Enrique Arizón Benito <enrique.arizonbenito@gmail.com>
2021-06-05 13:05:47 +0200

    setup 8081 -> /IT_notes , 8080 -> /

--- httpServer.py
+++ httpServer.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
+from os import chdir,fork
+
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
-httpd = HTTPServer( ('127.0.0.1',8000), SimpleHTTPRequestHandler )
-
+newpid = fork()
+if newpid != 0:
+   chdir('..')
+   port = 8001
+else:
+   port = 8000
+httpd = HTTPServer( ('127.0.0.1',port), SimpleHTTPRequestHandler )
 httpd.serve_forever()
